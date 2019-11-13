a5504c7a2fcf975130c2168339f060c7c88ce218
HWooKim97 <rgd5454@naver.com>
2019-11-13 20:43:35 +0900

    fix node - save with datastruct

--- script/node.js
+++ script/node.js
@@ -1,27 +1,29 @@
 const dataStructZone = document.querySelector(".dataStructZone");
-const nodeDataStruct = [];
-const nodeButton = [];
-let nodeCount = 0;
+const node = {
+    datastruct : [],
+    button : [],
+    cnt : 0
+};
 
 function makeNodeDataStruct(){
-    nodeDataStruct[nodeCount] = document.createElement("li");
+    const li = document.createElement("li");
     const span = document.createElement("span");
-    span.innerText = nodeCount + ` - `;
-    nodeDataStruct[nodeCount].appendChild(span);
-    dataStructZone.appendChild(nodeDataStruct[nodeCount]);
+    span.innerText = node.cnt + ` - `;
+    li.appendChild(span);
+    dataStructZone.appendChild(li);
+
+    node.datastruct[node.cnt] = [2];
 }
 
 function makeNodeButton(x, y){
-    nodeButton[nodeCount] = document.createElement("button");
-    nodeButton[nodeCount].innerText = nodeCount;
-    nodeButton[nodeCount].classList.add(nodeCount);
-    nodeButton[nodeCount].classList.add("btnNode");
-    nodeButton[nodeCount].onclick = handleNodeClick;
-    nodeButton[nodeCount].style.left = x + `px`;
-    nodeButton[nodeCount].style.top = y + 120 + `px`;
+    node.button[node.cnt] = document.createElement("button");
+    node.button[node.cnt].innerText = node.cnt;
+    node.button[node.cnt].classList.add("btnNode");
+    node.button[node.cnt].onclick = handleNodeClick;
+    node.button[node.cnt].style.left = x + `px`;
+    node.button[node.cnt].style.top = y + 120 + `px`;
 
-    canvasZone.appendChild(nodeButton[nodeCount]);
-    nodeCount++;
+    canvasZone.appendChild(node.button[node.cnt]);
 }
 
 function makeNode(event){
@@ -33,5 +35,6 @@ function makeNode(event){
     if(x >= 25 && y >= 25 && x <= w - 20 && y <= h - 20){
         makeNodeButton(x, y);
         makeNodeDataStruct();
+        node.cnt++;
     }
 }
