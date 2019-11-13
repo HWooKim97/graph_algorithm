446cbbcd4117c70b312d466bf6c0cecfb0ca9695
HWooKim97 <rgd5454@naver.com>
2019-11-14 00:10:43 +0900

    get button

--- script/edge.js
+++ script/edge.js
@@ -1,3 +1,24 @@
+//undirect 무방향
+//direct 방향
+
+const weightZone = document.querySelector(".weightZone");
+
+let check = false;
+let start, end;
+
+function drawEdge(){
+
+}
+
 function handleNodeClick(event){
     event.preventDefault();
+    if(check === false){
+        start = this;
+        check = true;
+    }
+    else{
+        end = this;
+        drawEdge();
+        check = false;
+    }
 }
