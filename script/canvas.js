449349d9026abc7a90138f77521663b9c6020a6b
HWooKim97 <rgd5454@naver.com>
2019-11-13 15:26:09 +0900

    delete test

--- script/canvas.js
+++ script/canvas.js
@@ -3,7 +3,6 @@ const graphCTX = graphCanvas.getContext("2d");
 const canvasZone = graphCanvas.parentElement;
 
 function setDefaultCanvas(){
-    console.log(graphCanvas.parentElement);
     graphCanvas.width = parseInt(window.getComputedStyle(canvasZone).width);
     graphCanvas.height = parseInt(window.getComputedStyle(canvasZone).height);
     graphCanvas.style.border = `1px solid black`;
