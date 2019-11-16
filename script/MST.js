9775c340c66da2b200800b2633d0fc27c44689a9
HWooKim97 <rgd5454@naver.com>
2019-11-17 00:49:07 +0900

    add mst.js

---
+++ script/MST.js
@@ -0,0 +1,38 @@
+let weightSum;
+
+function drawAgain(edge) {
+    const sBtn = document.querySelector(".vertex" + edge.s);
+    const eBtn = document.querySelector(".vertex" + edge.e);
+
+    const sx = parseInt(sBtn.style.left);
+    const sy = parseInt(sBtn.style.top) - 120;
+    const ex = parseInt(eBtn.style.left);
+    const ey = parseInt(eBtn.style.top) - 120;
+    
+    graphCTX.beginPath();
+    graphCTX.moveTo(sx, sy);
+    graphCTX.lineTo(ex, ey);
+    graphCTX.stroke();
+}
+
+function mstPrint(edge) {
+    const div = document.createElement("div");
+    div.innerText = "edge(" + edge.s + "," + edge.e + ")(weight : " + edge.w + ")";
+    weightResult.appendChild(div);
+}
+
+function mstInit(n){
+    graphCTX.clearRect(0, 0, graphCanvas.width, graphCanvas.height);
+    while(weightResult.lastChild != null)
+        weightResult.removeChild(weightResult.lastChild);
+    
+    seenVertex = [];
+    cycleGroup = [];
+    cycleGroup[0] = [];
+    cycleCnt = 0;
+    weightSum = 0;
+
+    if(n == 1) kruskalInit();
+    else if(n == 2) primInit();
+    else if(n == 3) sollinInit();
+}
