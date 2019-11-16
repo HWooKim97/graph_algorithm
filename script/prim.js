f624ebc1f6d2ff397562ca148580b215f6e705da
HWooKim97 <rgd5454@naver.com>
2019-11-17 01:59:39 +0900

    complete prim.js

--- script/prim.js
+++ script/prim.js
@@ -1,7 +1,45 @@
-function prim(){
+let treeVertex = [];
+let seenWeight = [];
 
+function primCheck(v){
+    for(let i = 0; i < treeVertex.length; i++){
+        if(treeVertex[i] === v) return false;
+    }
+    return true;
 }
 
-function primInit(){
-    
+function primInit(s){
+    treeVertex = [];
+    seenWeight = [];
+
+    do{
+        for(let r = s + 1; r < vertexCnt; r++){
+            if(!primCheck(r)) continue;
+            const w = document.querySelector(".tr" + r + "td" + s).innerText;
+            if(w != `o` && w != `x`){
+                seenWeight.push({
+                    w : parseInt(w),
+                    s : s,
+                    e : r
+                });
+            }
+        }
+        for(let c = 0; c < s; c++){
+            if(!primCheck(c)) continue;
+            const w = document.querySelector(".tr" + s + "td" + c).innerText;
+            if(w != `o` && w != `x`){
+                seenWeight.push({
+                    w : parseInt(w),
+                    s : s,
+                    e : c
+                });
+            }
+        }
+        seenWeight.sort(function(a, b) {
+            return a.w - b.w;
+        });
+        treeVertex.push(s);
+        s = seenWeight[0].e;
+        mstAddEdge(seenWeight.shift());
+    }while(seenWeight.length > 0) 
 }
