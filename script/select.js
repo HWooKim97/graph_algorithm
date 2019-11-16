76adc3d45c8ccb590f1a2115143a2df9e77b9347
HWooKim97 <rgd5454@naver.com>
2019-11-16 22:14:39 +0900

    fix design

--- script/select.js
+++ script/select.js
@@ -1,15 +1,36 @@
 const select = document.querySelector(".select");
 const weightResult = document.querySelector(".weightResult");
-const startInput = document.querySelector(".startInput");
+const leftStartInput = document.querySelector(".leftStartInput");
+const rightStartInput = document.querySelector(".rightStartInput");
+const startBtn = document.querySelector(".startBtn");
+const inputDiv = document.querySelector(".inputDiv");
+const btnDiv = document.querySelector(".btnDiv");
 
 let selection = 0;
-const ALGORITHMS = [
+const ALGORITHMS = [ null,
     kruskalInit, primInit, sollinInit,
     djikstraInit, bellmanInit, floydInit
 ]
 
+function handleStartBtn(event){
 
-function handleStartInput(event){
+}
+
+function handleRightStartInput(event){
+    if(event.keyCode === 13){
+        event.preventDefault();
+        const startVertex = parseInt(this.value);
+
+        if(startVertex >= 0 && startVertex < vertexCnt){
+            if(selection > 0 && selection < 6)
+                ALGORITHMS[selection](startVertex);
+            else if(selection == 6) ALGORITHMS[selection]();
+        }
+        else window.alert("Start Vertex Number Error!");
+    }
+}
+
+function handleLeftStartInput(event){
     if(event.keyCode === 13){
         event.preventDefault();
         const startVertex = parseInt(this.value);
@@ -17,9 +38,6 @@ function handleStartInput(event){
         if(startVertex >= 0 && startVertex < vertexCnt){
             bfsInit(startVertex);
             dfsInit(startVertex);
-            if(selection > 0 && selection < 6)
-                ALGORITHMS[selection](startVertex);
-            else if(selection == 6) ALGORITHMS[selection]();
         }
         else window.alert("Start Vertex Number Error!");
     }
@@ -30,14 +48,25 @@ function handleSelectChange(){
 
     weightResult.classList.remove("hiding");
     weightResult.classList.add("showing");
+    inputDiv.classList.remove("hiding");
+    inputDiv.classList.add("showing");
+    btnDiv.classList.remove("showing");
+    btnDiv.classList.add("hiding");
 
     if(selectValue == 1) selection = 1;
     else if(selectValue == 2) selection = 2;
     else if(selectValue == 3) selection = 3;
     else if(selectValue == 4) selection = 4;
     else if(selectValue == 5) selection = 5;
-    else if(selectValue == 6) selection = 6;
+    else if(selectValue == 6){
+        inputDiv.classList.remove("showing");
+        inputDiv.classList.add("hiding");
+        btnDiv.classList.remove("hiding");
+        btnDiv.classList.add("showing");
+        selection = 6;
+    }
     else{
+        selection = 0;
         weightResult.classList.remove("showing");
         weightResult.classList.add("hiding");
     }
@@ -45,5 +74,7 @@ function handleSelectChange(){
 
 function selectInit(){
     select.onchange = handleSelectChange;
-    startInput.onkeypress = handleStartInput;
+    leftStartInput.onkeypress = handleLeftStartInput;
+    rightStartInput.onkeypress = handleRightStartInput;
+    startBtn.onclick = handleStartBtn;
 }
