118e5a13f9d370bd2cecfd3cdfd43b5652e65e7a
HWooKim97 <rgd5454@naver.com>
2019-11-17 00:50:48 +0900

    complete kruskal.js

--- script/kruskal.js
+++ script/kruskal.js
@@ -1,12 +1,65 @@
-function kruskal(){
-    
-}
+function kruskal(weight){
+    let result = [];
+    let resultCnt = 0;
 
-function kruskalInit(start){
-    const weight = [];
-    for(let r = 0; r < vertexCnt; r++){
-        for(let c = 0; c < vertexCnt; c++){
+    while(weight.length > 0){
+        let edge = weight.shift();
+        if(checkCycle(edge) === true){
+            mstPrint(edge);
+            drawAgain(edge);
+            weightSum += edge.w;
+            result[resultCnt++] = {
+                s : edge.s,
+                e : edge.e
+            }
 
+            if(!checkVertex(edge.s) && checkVertex(edge.e)){ //s가 이미 있는 경우
+                seenVertex.push(edge.e);
+                cycleGroup[checkGroup(edge.s)].push(edge.e);
+            }
+            else if(checkVertex(edge.s) && !checkVertex(edge.e)){ //e가 이미 있는 경우
+                seenVertex.push(edge.s);
+                cycleGroup[checkGroup(edge.e)].push(edge.s);
+            }
+            else if(!checkVertex(edge.s) && !checkVertex(edge.e)){ //둘 다 이미 있지만 그룹이 다른 경우
+                console.log("do concat")
+                concatGroup(edge);
+            }
+            else{
+                seenVertex.push(edge.s);
+                seenVertex.push(edge.e);
+                cycleGroup[cycleCnt].push(edge.s);
+                cycleGroup[cycleCnt++].push(edge.e);
+                cycleGroup[cycleCnt] = [];
+            }
         }
     }
+}
+
+function kruskalInit(){
+    const weight = [];
+    let weightCnt = 0;
+    
+    for(let r = 0; r < vertexCnt; r++){
+        for(let c = 0; c < r; c++){
+            const w = document.querySelector(".tr" + r + "td" + c).innerText;
+            if(w != `o` && w != `x`){
+                weight[weightCnt++] = {
+                    w : parseInt(w),
+                    s : r,
+                    e : c
+                }
+            }
+        }
+    }
+
+    weight.sort(function(a, b) {
+        return a.w - b.w;
+    });
+
+    kruskal(weight);
+
+    const div = document.createElement("div");
+    div.innerText = "Sum of Weight : " + weightSum;
+    weightResult.appendChild(div);
 }
